2e23623169a40227b0de16cc37ac9e47140b3008
bananaback <58079308+bananaback@users.noreply.github.com>
2023-05-28 17:08:45 +0700

    add project page

---
+++ UI/Manager/ProjectPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using CompanyManagement.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CompanyManagement.UI.Manager
+{
+    /// <summary>
+    /// Interaction logic for ProjectPage.xaml
+    /// </summary>
+    public partial class ProjectPage : Page
+    {
+        public ProjectPage()
+        {
+            InitializeComponent();
+            this.DataContext = new ProjectPageViewModel();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //var projectEditorWindow = new ProjectForm(this.DataContext as ProjectsPageViewModel);
+            //projectEditorWindow.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            btn.Command.Execute(btn.CommandParameter);
+            //NavigationService.Navigate(new StagesPage());
+        }
+
+        private void ViewMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new StagesPage());
+        }
+
+        private void Button_PreviewMouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var btn = sender as Button;
+            btn.Command.Execute(btn.CommandParameter);
+        }
+    }
+}
