97d0a78aa0222482083fea8d662a7319c2e1fee9
bananaback <58079308+bananaback@users.noreply.github.com>
2023-05-28 17:23:23 +0700

    add wrappers

---
+++ Wrappers/ProjectWrapper.cs
@@ -0,0 +1,16 @@
+﻿using CompanyManagement.EF;
+
+namespace CompanyManagement.Wrappers
+{
+    public class ProjectWrapper : Wrapper
+    {
+        private readonly Project _project;
+        public ProjectWrapper(Project project) : base()
+        {
+            _project = project;
+        }
+        public string ID => _project.ID;
+        public string Name => _project.Name;
+        public string Description => _project.Description;
+    }
+}
